663e7e5b8dbcf70e79526bb17b5e204896dfc2d6
Charlie Brown <c.florestal.com>
2018-05-21 12:49:55 -0400

    restructure templates

--- web/loader.php
+++ web/loader.php
@@ -1,7 +1,11 @@
  <?php 
 	require_once '../vendor/autoload.php';
 
-	$loader = new Twig_Loader_Filesystem('../templates');
+	$loader1 = new Twig_Loader_Filesystem('../templates');
+	$loader2 = new Twig_Loader_Filesystem('../templates/home');
+	$loader3 = new Twig_Loader_Filesystem('../templates/profile');
+
+	$loader = new Twig_Loader_Chain(array($loader1,$loader2,$loader3));
 
 	$twig = new Twig_Environment($loader);
 
