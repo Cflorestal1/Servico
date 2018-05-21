0aa6fa5ac7e0ec0e6e0c82d1130c5064fc15b24d
Charlie Brown <c.florestal.com>
2018-05-21 15:56:33 -0400

    restructure path for css and js

--- web/index.php
+++ web/index.php
@@ -2,12 +2,39 @@
 	include 'loader.php';
 
 	$nav = ['Home','Explore','Log In'];
-	$styles = ['index','home','header','footer','modal'];
-	$scripts = ['index','home','modal','functions'];
+	$styles =[ 
+		'path' => [
+			'base'	=> '/Servico/web/css/',
+			'home' => '/Servico/web/css/home/',
+			'user'  => '/Servico/web/css/user/'
+		], 
+	
+		'files' => [
+			'base' => ['index.css','footer.css'],
+			'home' => ['header-home.css','home.css','modal.css'],
+			'user' => ['header-user.css','user.css']
+		]
+	];
+
+	$scripts = [
+		'path' => [
+			'base' => '/Servico/web/js/',
+			'home' => '/Servico/web/js/home/',
+			'user' => '/Servico/web/js/user/'
+		],
+		
+		'files' => [
+			'base' => ['functions.js','index.js'],
+			'home' => ['home.js','modal.js'],
+			'user' => ['user.js']
+		]
+	];
 	
 	
 	$base = [ 'title' => 'Base'];
 	
+	$cusClass = ['img6' => 'img-responsive col-lg-'];
+
 	$home = [
 		'title' => 'Servico',
 		'logo' => '/Servico/web/img/logo.png',
